e78e7020eec2491cda9e29c4bf9b9a5c6971780f
LoliNakaze <LoliNakaze@users.noreply.github.com>
2017-05-02 00:55:51 +0200

    New contains

--- OperationShape.cpp
+++ OperationShape.cpp
@@ -2,13 +2,32 @@
 #include <iostream>
 #include <math.h>
 
+bool OperationShape::test_contains(G3Xhmat inv1, G3Xhmat mat2, std::vector<Point *> points1,
+                                   std::vector<Point *> points2, bool b) {
+    for (int i = 0; i < points2.size(); i++) {
+        G3Xpoint origin = {points2[i]->get_x(), points2[i]->get_y(), points2[i]->get_z()};
+        G3Xpoint scaled = {0, 0, 0};
+        G3Xpoint res = {0, 0, 0};
+
+        g3x_ProdHMatPoint(mat2, origin, scaled);
+        g3x_ProdHMatPoint(inv1, scaled, res);
+
+        points2[i]->set_visibility(b);
+    }
+}
+
 OperationShape::OperationShape(Shape *s1, Shape *s2, OperationType ot)
         : shape1(s1), shape2(s2), operationType(ot) {
 
+    std::vector<Point *> points1;
+    std::vector<Point *> points2;
+
     switch (ot) {
         case INTERSECTION:
-            std::vector<Point *> points2 = s2->get_points();
+            points2 = s2->get_points();
             for (int i = 0; i < points2.size(); i++) {
+                g3x_ProdHMatPoint()
+
                 G3Xpoint tmp = {points2[i]->get_x(), points2[i]->get_y(), points2[i]->get_z()};
                 G3Xpoint res = {0, 0, 0};
 
@@ -17,7 +36,7 @@ OperationShape::OperationShape(Shape *s1, Shape *s2, OperationType ot)
                 points2[i]->set_visibility(s1->contains(Point(res[0], res[1], res[2])));
             }
 
-            std::vector<Point *> points1 = s1->get_points();
+            points1 = s1->get_points();
             for (int i = 0; i < points1.size(); i++) {
                 G3Xpoint tmp = {points1[i]->get_x(), points1[i]->get_y(), points1[i]->get_z()};
                 G3Xpoint res = {0, 0, 0};
@@ -27,33 +46,91 @@ OperationShape::OperationShape(Shape *s1, Shape *s2, OperationType ot)
                 points1[i]->set_visibility(s2->contains(Point(res[0], res[1], res[2])));
             }
             break;
-/*        case UNION:
+        case UNION:
+            points2 = s2->get_points();
+            for (int i = 0; i < points2.size(); i++) {
+                G3Xpoint tmp = {points2[i]->get_x(), points2[i]->get_y(), points2[i]->get_z()};
+                G3Xpoint res = {0, 0, 0};
+
+                g3x_ProdHMatPoint(s1->matrice_transformation_inverse, tmp, res);
+
+                points2[i]->set_visibility(!s1->contains(Point(res[0], res[1], res[2])));
+            }
+
+            points1 = s1->get_points();
+            for (int i = 0; i < points1.size(); i++) {
+                G3Xpoint tmp = {points1[i]->get_x(), points1[i]->get_y(), points1[i]->get_z()};
+                G3Xpoint res = {0, 0, 0};
+
+                g3x_ProdHMatPoint(s2->matrice_transformation_inverse, tmp, res);
+
+                points1[i]->set_visibility(!s2->contains(Point(res[0], res[1], res[2])));
+            }
             break;
         case SUBTRACTION:
-            break;*/
+            points1 = s1->get_points();
+            for (int i = 0; i < points1.size(); i++) {
+                G3Xpoint tmp = {points1[i]->get_x(), points1[i]->get_y(), points1[i]->get_z()};
+                G3Xpoint res = {0, 0, 0};
+
+                g3x_ProdHMatPoint(s2->matrice_transformation_inverse, tmp, res);
+
+                points1[i]->set_visibility(!s2->contains(Point(res[0], res[1], res[2])));
+            }
+
+            points2 = s2->get_points();
+            for (int i = 0; i < points2.size(); i++) {
+                points2[i]->set_visibility(false);
+            }
+            break;
     }
 }
 
 OperationShape::~OperationShape() {
 }
 
-bool OperationShape::contains(const Point &p) const {
-    switch (operationType) {
-        case INTERSECTION:
-            return shape1->contains(p) && shape2->contains(p);
-        case UNION:
-            return shape1->contains(p) || shape2->contains(p);
-        case SUBTRACTION:
-            return shape1->contains(p) && !shape2->contains(p);
+bool OperationShape::contains_aux(G3Xhmat mat, Shape *shape, const Point &p) const {
+    G3Xhmat tmp;
+    CanonicShape *cshape;
+    TransformationShape *tshape;
+    OperationShape *oshape;
+
+    if (cshape = dynamic_cast<CanonicShape *>(shape)) {
+        g3x_ProdHMat(mat, cshape->matrice_transformation_inverse, tmp);
+        G3Xpoint point = {0, 0, 0};
+        g3x_ProdHMatPoint(tmp, {p.get_x(), p.get_y(), p.get_z()}, point);
+        return cshape->contains(p);
+    } else if (tshape = dynamic_cast<TransformationShape *>(shape)) {
+        return contains_aux(mat, tshape->origin_shape(), p);
+    } else if (oshape = dynamic_cast<OperationShape *>(shape)) { // OperationShape
+        g3x_ProdHMat(mat, oshape->matrice_transformation_inverse, tmp);
+        switch (operationType) {
+            case UNION:
+                return contains_aux(tmp, oshape->shape1, p) || contains_aux(tmp, oshape->shape2, p);
+            case INTERSECTION:
+                return contains_aux(tmp, oshape->shape1, p) && contains_aux(tmp, oshape->shape2, p);
+            case SUBTRACTION:
+                return contains_aux(tmp, oshape->shape1, p) && !contains_aux(tmp, oshape->shape2, p);
+            default:
+                cerr << "Unknown operation type : " << operationType << endl;
+                return false;
+        }
+    } else {
+        cerr << "Unknown shape type in contains_aux" << endl;
+        return false;
     }
 }
 
+bool OperationShape::contains(const Point &p) const {
+    G3Xhmat mat;
+    g3x_MakeIdentity(mat);
+
+    return contains_aux(mat, this, p);
+}
+
 void OperationShape::draw() const {
-    std::cerr << "Shape 1 draw" << std::endl;
     shape1->draw();
-    std::cerr << "Shape 2 draw" << std::endl;
     shape2->draw();
-    std::cerr << "End draw" << std::endl;
 }
 
 std::vector<Point *> OperationShape::get_points() const {
--- OperationShape.hpp
+++ OperationShape.hpp
@@ -4,24 +4,27 @@
 #include <g3x.h>
 #include "Shape.hpp"
 
-enum OperationType {UNION, INTERSECTION, SUBTRACTION};
+enum OperationType {
+    UNION, INTERSECTION, SUBTRACTION
+};
 
 class OperationShape : public Shape {
 public:
-    OperationShape(Shape* shape1, Shape* shape2, OperationType type); /* Constructeur */
+    OperationShape(Shape *shape1, Shape *shape2, OperationType type); /* Constructeur */
     ~OperationShape(); /* Destructeur */
 
     bool contains(const Point &p) const;
 
-	void draw() const;
+    void draw() const;
 
-	std::vector<Point*> get_points() const ;
+    std::vector<Point *> get_points() const;
 
-    
-protected:
-	Shape* shape1;    
-	Shape* shape2;    
-	OperationType operationType;
+private:
+    Shape *shape1;
+    Shape *shape2;
+    OperationType operationType;
+
+    bool contains_aux(G3Xhmat mat, Shape* shape, const Point &p) const;
 };
 
 #endif
